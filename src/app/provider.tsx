8bd93dc1d13e3694d73ad269f319e1e12801f119
rudra <goutam.prasad.2707@gmail.com>
2026-02-09 17:16:51 +0530

    feat : client side provider

---
+++ src/app/provider.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { AppSidebar } from "@/components/app-sidebar";
+import { Footer, Navigation } from "@/components/layout";
+import { ThemeProvider } from "@/components/theme-provider";
+import {
+	SidebarInset,
+	SidebarProvider,
+	SidebarTrigger,
+} from "@/components/ui/sidebar";
+import { AppProvider } from "@/context";
+import { usePathname } from "next/navigation";
+import type { ReactNode } from "react";
+
+export default function LayoutProvider({ children }: { children: ReactNode }) {
+	const pathname: string = usePathname();
+
+	// Define paths that should NOT show the Sidebar
+	const noSidebarPaths = ["/", "/login", "/register"];
+	const hideSidebar = noSidebarPaths.includes(pathname);
+
+	return (
+		<AppProvider>
+			<ThemeProvider attribute="class" defaultTheme="dark">
+				{hideSidebar ? (
+					/* Landing / Auth Layout (No Sidebar) */
+					<div className="flex min-h-screen flex-col">
+						<Navigation />
+						<main className="flex-1 mt-16">{children}</main>
+						<Footer />
+					</div>
+				) : (
+					/* Dashboard/App Layout (With Sidebar) */
+					<SidebarProvider>
+						<AppSidebar />
+						<SidebarInset>
+							<header className="flex h-16 shrink-0 items-center gap-2 px-4">
+								<SidebarTrigger className="-ml-2 -mt-2" />
+							</header>
+							<div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+								{children}
+							</div>
+							<Footer />
+						</SidebarInset>
+					</SidebarProvider>
+				)}
+			</ThemeProvider>
+		</AppProvider>
+	);
+}
