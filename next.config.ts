0cd49434ebb57eb3430989def95fe91ca4e41938
rudra <goutam.prasad.2707@gmail.com>
2026-03-01 21:31:34 +0530

    chore : allowedDevOrigins & remotePatterns added

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+	reactCompiler: true,
+	
+	allowedDevOrigins: ["local-origin.dev", "*.local-origin.dev"],
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "api.dicebear.com",
+				pathname: "/9.x/**",
+			},
+		],
+	},
 };
 
 export default nextConfig;
