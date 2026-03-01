d22f9fae216583126452f010fded845a37eb69b4
rudra <goutam.prasad.2707@gmail.com>
2026-03-01 21:32:33 +0530

    chore : metadata added

--- src/app/(app)/(settings)/layout.tsx
+++ src/app/(app)/(settings)/layout.tsx
@@ -2,7 +2,10 @@ import type { Metadata } from "next";
 import type { ReactNode } from "react";
 
 export const metadata: Metadata = {
-	title: "Settings",
+	title: {
+    template: "%s | Settings", // %s is replaced by the child page title
+    default: "Settings",
+  },
 };
 
 export default function SettingLayout({ children }: { children: ReactNode }) {
